fd39ca879e3e0d39e3a69600dccafc68e9241eab
Evgeniy Morozov <zhe3145@yandex.ru>
2017-03-24 10:28:28 +0300

    ANSI C fixes

--- FeistelNetEcryption/FeistelNetEcryption/main.c
+++ FeistelNetEcryption/FeistelNetEcryption/main.c
@@ -15,17 +15,21 @@
 
 int main(int argc, const char * argv[])
 {
-    char message[256] = "1234567";
+    feist_ctx_t * context = NULL;
+    char message[256]   = "1234567";
     size_t message_size = 0;
-    char * encrypted = NULL;
-    char * decrypted = NULL;
+    char * encrypted    = NULL;
+    char * decrypted    = NULL;
+    
     printf("Input message > ");
     scanf("%[^\n]s", message);
     message_size = strlen(message);
-    feist_ctx_t * context = feist_init(10, 0xDEAD, NULL, NULL);
     printf("Source message: %s\n", message);
+    
+    context = feist_init(10, 0xDEAD, NULL, NULL);
     encrypted = feist_encrypt(context, message, &message_size);
     printf("Encrypted message: %s\n", encrypted);
+    
     decrypted = feist_decrypt(context, encrypted, &message_size);
     printf("Decrypted message: %s\n", decrypted);
     
@@ -33,5 +37,6 @@ int main(int argc, const char * argv[])
     free(encrypted);
     free(decrypted);
     free(context);
+    
     return 0;
 }
